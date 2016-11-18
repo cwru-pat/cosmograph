5a292c030be8c4f199534f033402da6c8ac1a875
Jim Mertens <james@ann.kenyon.edu>
2016-11-18 01:10:29 -0500

    clean up some params, etc

--- scripts/dust_test.sh
+++ scripts/dust_test.sh
@@ -31,17 +31,17 @@ do_runs()
 
 cmake -DCOSMO_N=20 .. && make -j32
 sed -i -E "s/steps = [0-9]+/steps = 6000/g" $TMP_CONFIG_FILE
-sed -i -E "s/output_dir = long_dust_test/output_dir = long_dust_test_r16/g" $TMP_CONFIG_FILE
+sed -i -E "s/output_dir = long_dust_test/output_dir = long_dust_test_r20/g" $TMP_CONFIG_FILE
 do_runs
 
 cmake -DCOSMO_N=30 .. && make -j32
 sed -i -E "s/steps = [0-9]+/steps = 9000/g" $TMP_CONFIG_FILE
-sed -i -E "s/output_dir = long_dust_test_r16/output_dir = long_dust_test_r32/g" $TMP_CONFIG_FILE
+sed -i -E "s/output_dir = long_dust_test_r20/output_dir = long_dust_test_r30/g" $TMP_CONFIG_FILE
 do_runs
 
 cmake -DCOSMO_N=40 .. && make -j32
 sed -i -E "s/steps = [0-9]+/steps = 12000/g" $TMP_CONFIG_FILE
-sed -i -E "s/output_dir = long_dust_test_r32/output_dir = long_dust_test_r64/g" $TMP_CONFIG_FILE
+sed -i -E "s/output_dir = long_dust_test_r30/output_dir = long_dust_test_r4/g" $TMP_CONFIG_FILE
 do_runs
 
 rm $TMP_CONFIG_FILE
